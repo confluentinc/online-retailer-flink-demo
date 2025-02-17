c6084b1f02ed3e29fad385e7db81cc372c12a963
Ahmed Zamzam <31252819+AhmedsZamzam@users.noreply.github.com>
2025-02-17 23:20:19 +0000

    Update SalesDataGen.java
    Changed the payments datagen to start at order_id 2500

--- code/payments-app/src/main/java/io/confluent/examples/datacontract/datagen/SalesDataGen.java
+++ code/payments-app/src/main/java/io/confluent/examples/datacontract/datagen/SalesDataGen.java
@@ -12,7 +12,7 @@ public class SalesDataGen {
     private static final String CONF_CODE_CHAR_LIST = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final int CONF_CODE_LENGTH = 8;
 
-    private static int orderId = 198;
+    private static int orderId = 2500;
 
     static int getRandomNumber(int max)
     {
