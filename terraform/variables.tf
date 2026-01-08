2880baddb47c1094de54123f46c865a87f4ef850
Kyle K <kklein@confluent.io>
2026-01-08 08:02:22 -0700

    remove redshift and snowflake, make  minor text revisions, remove LAB3 (#70)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,69 +3,37 @@ variable "email" {
   type        = string
 }
 
-
 variable "prefix" {
   description = "Prefix for resource names"
   type        = string
   default     = "shiftleft"
 }
 
-variable "cloud_region"{
+variable "cloud_region" {
   description = "AWS Cloud Region"
   type        = string
-  default     = "us-east-1"    
+  default     = "us-east-1"
 }
 
-variable "db_username"{
+variable "db_username" {
   description = "Postgres DB username"
   type        = string
-  default     = "postgres"  
+  default     = "postgres"
 }
 
-variable "db_password"{
+variable "db_password" {
   description = "Postgres DB password"
   type        = string
-  default     = "Admin123456!!"  
+  default     = "Admin123456!!"
 }
 
 
-variable "confluent_cloud_api_key"{
-    description = "Confluent Cloud API Key"
-    type        = string
-}
-
-variable "confluent_cloud_api_secret"{
-    description = "Confluent Cloud API Secret"
-    type        = string     
-}
-
-variable "data_warehouse" {
-  description = "Type of data warehouse to use (either 'redshift' or 'snowflake')"
+variable "confluent_cloud_api_key" {
+  description = "Confluent Cloud API Key"
   type        = string
-  default     = "redshift"
-  validation {
-    condition     = contains(["redshift", "snowflake"], var.data_warehouse)
-    error_message = "The data_warehouse variable must be either 'redshift' or 'snowflake'."
-  }
 }
 
-variable "snowflake_account" {
-  description = "Snowflake account identifier"
+variable "confluent_cloud_api_secret" {
+  description = "Confluent Cloud API Secret"
   type        = string
-  default     = "redshift_selected"
 }
-
-variable "snowflake_username" {
-  description = "Snowflake username"
-  type        = string
-  default     = ""
-}
-
-variable "snowflake_password" {
-  description = "Snowflake password"
-  type        = string
-  default     = ""
-}
-
-
-
